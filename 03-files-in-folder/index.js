610968ceac3d94e306f92d68da39140aada09491
Svetlana Evdokimova <7757331@gmail.com>
2025-01-03 15:12:39 +0300

    feat: implement 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const file = path.join(__dirname, 'secret-folder');
+fs.readdir(file, { withFileTypes: true }, (error, files) => {
+  console.log('\nCurrent directory files:');
+  if (error) console.log(error);
+  else {
+    files.forEach((item) => {
+      let current = item.name.split('.');
+      if (!item.isDirectory()) {
+        const pathsToCheck = path.join(file, item.name);
+        fs.stat(pathsToCheck, (error, stats) => {
+          if (error) console.log(error);
+          else {
+            console.log(
+              `${current[0]} -  ${path.extname(item.name).slice(1)} - ${
+                stats.size / 1000
+              } kb`,
+            );
+          }
+        });
+      }
+    });
+  }
+});
